0adcad1d522de6a62b0d7c638f6ae7ca2b70d228
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-03-23 19:43:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+let formData = {
+  name: "Stu",
+  email: "Fleis"
+};
+
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+
+
+function submitData() {
+  fetch("http://localhost:3000/users", configObj)
+  .then(resp => resp.json())
+  .then(json => {
+    const p = document.createElement('p')
+    for (const j in json) {
+      p.innerHTML = json[j]
+      document.body.appendChild(p)
+    }
+  }) 
+}
+
+submitData()
