b790b856bcd269c1251bcf9dde1489d6e0722928
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-03-23 20:19:56 -0400

    commit tree b2145cc7c412e504c10edb7bcaa42e188dd843c1

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 // Add your code here
 
-let formData = {
-  name: "Stu",
-  email: "Fleis"
-};
-
-let configObj = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(formData)
-};
 
 
-function submitData() {
-  fetch("http://localhost:3000/users", configObj)
+function submitData(name, email) {
+  let formData = {
+    name,
+    email
+  };
+  
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
   .then(resp => resp.json())
   .then(json => {
-    const p = document.createElement('p')
-    for (const j in json) {
-      p.innerHTML = json[j]
-      document.body.appendChild(p)
+    const h1 = document.createElement('h1')
+    const div = document.createElement('div')
+    h1.innerHTML = json['id']
+    document.body.appendChild(div)
+    div.appendChild(h1)
     }
-  }) 
+  ) 
+  .catch(function(error) {
+    const h2 = document.createElement('h2')
+    h2.innerText = error.message
+    document.body.appendChild(h2)
+  })
 }
 
-submitData()
+ submitData()
